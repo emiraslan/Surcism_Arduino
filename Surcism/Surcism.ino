6d8e1685270cec890eecc948144d4d4379b2b227
Emiraslan <aslanbaxisli.ba@gmail.com>
2017-11-05 03:26:30 +0200

    Removed additional functionality to .h files

--- Surcism/Surcism.ino
+++ Surcism/Surcism.ino
@@ -1,105 +1,26 @@
-const short M1F = 6;
-const short M2F = 9;
-const short M1R = 10;
-const short M2R = 11;
+// Written and copyrighted by (Alphabetical order)
+//    Amiraslan Bakhshili
+//    Eltun Ibrahimov
+//    Gulshan Hajibalayeva
+//    Madina Shukurlu
+//    Ramziyya Garazade
 
-const short leftDisplay = 2;
-const short middleDisplay = 3;
-const short rightDisplay = 4;
+char cur_state = 'u';
 
-int M1E = 8; //Todo: Change this pincwith transistor
-int M2E = 8; // Do it with transistors and battery! Pin voltage will be enough for operating a transistor
+#include "Globals.h"
+#include "MotorControl.h"
+#include "Infrared.h"
+#include "Ultrasonic.h"
 
-const short initial_delay = 10; 
-const short action_delay = 3000; 
-const short input_delay = 1;
-const short motor_delay = 10; 
-
-const short left = A0;
-const short middle = A1;
-const short right = A2;
-
-const bool debug_mode = true;
-
-float barrier = 558;
-
-float left_barrier = 580;
-float middle_barrier = 480;
-float right_barrier = 540;
-
-short M1_move = 100;
-short M2_move = 100;
-
-short M1_move_s = 35;
-short M2_move_s = 35;
-
-short M1_stop = 0;
-short M2_stop = 0;
-
-const char left_dir = 'l';
-const char right_dir = 'r';
-const char forward_dir = 'f';
-const char backward_dir = 'b';
-
-char cur_pos = forward_dir;
-char prev_pos = forward_dir;
-
-/*
- directions:
-       left = 'l'
-       right = 'r'
-       forward = 'f'
-       background = 'b' 
-*/
-
-class SensorData{
-  public: 
-    short left_data;
-    short middle_data;
-    short right_data;  
-
-    void displayData()
-    {
-       Serial.print("Left: ");
-       Serial.print(left_data);
-       Serial.print("\tMiddle: ");
-       Serial.print(middle_data);
-       Serial.print("\tRight: ");
-       Serial.print(right_data);
-       
-       Serial.print("\tLeft-bar: ");
-       Serial.print(left_barrier);
-       Serial.print("\tMiddle-bar: ");
-       Serial.print(middle_barrier);
-       Serial.print("\tRight-bar: ");
-       Serial.println(right_barrier);
-    }
-
-    bool isLeftBlack(){
-      if(left_data > left_barrier){
-          return true;
-      }
-      return false;
-    }
-
-    bool isMiddleBlack(){
-      if(middle_data > middle_barrier){
-          return true;
-      }
-      return false;
-    }
-
-    bool isRightBlack(){
-      if(right_data > right_barrier){
-          return true;
-      }
-      return false;
-    }
-};
+#define infrared 'i'
+#define ultrasonic 'u'
+#define remote_control 'r'
+#define self_balance 's'
 
 void setup() {
   Serial.begin(9600);
-  
+
+  // Infrared Line-Follower
   pinMode(M1F, OUTPUT);
   pinMode(M2F, OUTPUT);
   pinMode(M1R, OUTPUT);
@@ -112,158 +33,38 @@ void setup() {
   pinMode(leftDisplay, OUTPUT);
   pinMode(rightDisplay, OUTPUT);
 
+  // Ultrasonic 
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
+
   delay(action_delay);
+  
   startMotors();
 }
 
 void loop() {
-  calibrateWithPotentiometer();
-  
-  SensorData data = readData();
+  switch(cur_state){
+    case infrared:
+      startInfraredRobot();
+    break;
 
-  bool leftIsBlack = data.isLeftBlack();
-  bool middleIsBlack = data.isMiddleBlack();
-  bool rightIsBlack = data.isRightBlack();
-   
-  if(leftIsBlack && middleIsBlack && rightIsBlack){ //XXX
-    cur_pos = forward_dir;
-    checkForDelay();
-    goForward();
-  }else if(!leftIsBlack && !middleIsBlack && !rightIsBlack){ // OOO
-//    cur_pos = forward_dir;
-//    checkForDelay();
-//    goForward();
-  }else if(!leftIsBlack && middleIsBlack && !rightIsBlack){ // OXO
-    cur_pos = forward_dir;
-    checkForDelay();
-    goForward();
-  }
-  else if(leftIsBlack && !middleIsBlack && rightIsBlack){ // XOX
-    cur_pos = forward_dir;
-    checkForDelay();
-    goForward();
-  }
-  else if(leftIsBlack && !middleIsBlack && !rightIsBlack){ // XOO
-    cur_pos = left_dir;
-    checkForDelay();
-    goLeft();
-  }
-  else if(leftIsBlack && middleIsBlack && !rightIsBlack){ // XXO
-    cur_pos = left_dir;
-    checkForDelay();
-    goLeft();
-  }
-  else if(!leftIsBlack && middleIsBlack && rightIsBlack){ // OXX
-    cur_pos = right_dir;
-    checkForDelay();
-    goRight();
-  }
-  else if(!leftIsBlack && !middleIsBlack && rightIsBlack){ // OOX
-    cur_pos = right_dir;
-    checkForDelay();
-    goRight();
-  }
-  else{
-    cur_pos = backward_dir;
-    checkForDelay();
-    goBackward();
-  }
+    case ultrasonic: 
+     startUltrasonicRobot();
+    break;
+    
+    case remote_control:
+    //Todo: not implemented yet...
+    break;
 
-  if(middleIsBlack){
-    digitalWrite(middleDisplay, HIGH);
-  }else{
-    digitalWrite(middleDisplay, LOW);  
-  }
+    case self_balance: 
+    //Todo: not implemented yet...
+    break;
 
-  if(leftIsBlack){
-    digitalWrite(leftDisplay, HIGH);
-  }else{
-    digitalWrite(leftDisplay, LOW);  
-  }
-
-  if(rightIsBlack){
-    digitalWrite(rightDisplay, HIGH);
-  }else{
-    digitalWrite(rightDisplay, LOW);  
-  }
-
-  delay(input_delay);
-}
-
-
-//Todo: Optimize this function. Remove hard coded data.  
-void calibrateWithPotentiometer(){
- 
-  int sensorValue = analogRead(A3);
-  int sensorValue2 = analogRead(A4);
-  int sensorValue3 = analogRead(A5);
-  
-  left_barrier = sensorValue * (1000.0 / 1023.0);
-  middle_barrier = sensorValue2 * (1000.0 / 1023.0);
-  right_barrier = sensorValue3 * (1000.0 / 1023.0);
-}
-
-void goForward(){
-  analogWrite(M1F, M1_stop);
-  analogWrite(M2F, M2_stop);
-  analogWrite(M1R, M1_move);
-  analogWrite(M2R, M2_move);
-}
-
-void goBackward(){
-  analogWrite(M1F, M1_move);
-  analogWrite(M2F, M2_move);
-  analogWrite(M1R, M1_stop);
-  analogWrite(M2R, M2_stop); 
-}
-
-void goRight(){
-  analogWrite(M1F, M1_stop);
-  analogWrite(M2F, M2_stop);
-  analogWrite(M1R, M1_move_s);
-  analogWrite(M2R, M2_move); 
-}
-
-void goLeft(){
-  analogWrite(M1F, M1_stop);
-  analogWrite(M2F, M2_stop);
-  analogWrite(M1R, M1_move);
-  analogWrite(M2R, M2_move_s);     
-}
-
-void stopMotors(){
-  digitalWrite(M1E, LOW);
-  digitalWrite(M2E, LOW);
-}
-
-void startMotors(){
-  digitalWrite(M1E, HIGH);
-  digitalWrite(M2E, HIGH);
-}
-  
-SensorData readData(){
-  SensorData data;
-  
-  data.left_data = analogRead(left);
-  data.middle_data = analogRead(middle);
-  data.right_data = analogRead(right);
-
-  if(debug_mode){
-    data.displayData();
-  }
-
-  return data;
-}
-
-bool checkForDelay(){
-   if(prev_pos != cur_pos){
-      prev_pos = cur_pos;
-      makeMSCD(); // Motor State Change Delay  
-      return true;
+    default:
+      startUltrasonicRobot();
+    break;
     }
-    return false;
 }
 
-void makeMSCD(){
-   delay(motor_delay);
-}
+
+
