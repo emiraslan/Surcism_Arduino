7de4a7466630ea5d4cd7f67899de51099d0e6214
Emiraslan <aslanbaxisli.ba@gmail.com>
2017-11-05 03:31:28 +0200

    Created new Ultrasonic sensor functionality in a seperate file

---
+++ Surcism/Ultrasonic.h
@@ -0,0 +1,29 @@
+void startUltrasonicRobot() {
+  goBackward();
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+
+if(distance <= avioding_dist){
+	goForward();
+	delay(back_delay);
+	goLeft();
+	delay(turn_delay);
+	goBackward();
+}
+
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.print(distance);
+Serial.println(" cm");
+}
